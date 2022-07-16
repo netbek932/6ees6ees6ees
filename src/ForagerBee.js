baf4b32bebb9b6136458fbe6a80bef24d178725c
netbek932 <rjeren22@gmail.com>
2022-07-16 17:02:15 -0400

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor() {
+    super()
+    this.age = 10;
+    this.job = 'find pollen';
+    this.color = 'yellow';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    treasureChest.push(treasure);
+  }
 };
